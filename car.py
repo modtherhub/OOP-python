ba457128742dab512e723dd4d4643a05e523dd19
modtherhub <moibhub@gmail.com>
2025-05-24 23:49:39 +0400

    create calas &object

--- car.py
+++ car.py
@@ -1,8 +1,15 @@
 class Car:
 
-    make = None
-    model = None 
-    year = None
-    color = None  
+    
 
-    def drive(self)
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model 
+        self.year = year
+        self.color = color 
+
+    def drive(self):
+        print("This car is driving")
+
+    def stop(self):
+        print("This car is stopped")
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from car import Car
+
+car_1 = Car("Golf", "VW", 2024, "blue")
+
+print(car_1.color)
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+
+car_1.drive()
+car_1.stop()
